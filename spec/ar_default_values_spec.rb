15cf21aba2bad18c915e72683a2e5504e6f6d672
Yamada Masaki <masaki@hisme.net>
2012-04-03 03:47:05 +0900

    make sure to set instance values when using lambda

--- spec/ar_default_values_spec.rb
+++ spec/ar_default_values_spec.rb
@@ -8,8 +8,8 @@ end
 
 class Book2 < ActiveRecord::Base
   self.table_name = :books
-
-  default_values price: 0 do
+  attr_protected :edition
+  default_values price: 0, edition: 1 do
     t = Time.now
     {:released_at => t }
   end
@@ -17,47 +17,58 @@ end
 
 describe "ArDefaultValues" do
   subject { @sample }
-  before do
-    @now = Time.at(Time.now.to_i)
-    Time.stub(:now).and_return(@now)
-  end
-  context :without_attributes do
+  describe :fixed_default_values do
     before do
-      @sample = Book.new
+      @now = Time.at(Time.now.to_i)
+      Time.stub(:now).and_return(@now)
     end
-    its(:title) { should be_nil }
-    its(:price) { should == 0 }
-    its(:edition) { should == 1 }
-    its(:released_at) { should == @now }
-  end
-  context :with_attributes do
-    before do
-      @sample = Book.new(:price => 100)
-    end
-    its(:title) { should be_nil }
-    its(:price) { should == 100 }
-    its(:edition) { should == 1 }
-    its(:released_at) { should == @now }
-  end
-  context :with_block do
-    before do
-      @sample = Book.new do |t|
-        t.title = 'foo'
+    context :without_attributes do
+      before do
+        @sample = Book.new
       end
+      its(:title) { should be_nil }
+      its(:price) { should == 0 }
+      its(:edition) { should == 1 }
+      its(:released_at) { should == @now }
     end
-    its(:title) { should == 'foo' }
-    its(:price) { should == 0 }
-    its(:edition) { should == 1 }
-    its(:released_at) { should == @now }
+    context :with_attributes do
+      before do
+        @sample = Book.new(:price => 100)
+      end
+      its(:title) { should be_nil }
+      its(:price) { should == 100 }
+      its(:edition) { should == 1 }
+      its(:released_at) { should == @now }
+    end
+    context :with_block do
+      before do
+        @sample = Book.new do |t|
+          t.title = 'foo'
+        end
+      end
+      its(:title) { should == 'foo' }
+      its(:price) { should == 0 }
+      its(:edition) { should == 1 }
+      its(:released_at) { should == @now }
+    end
+
+    context :consider_attr_protected do
+      before do
+        @sample = Book.new(:edition => 3)
+      end
+      its(:title) { should be_nil }
+      its(:price) { should == 0 }
+      its(:edition) { should == 1 }
+      its(:released_at) { should == @now }
+    end
   end
 
-  context :consider_attr_protected do
+  describe :instance_values_with_block do
     before do
-      @sample = Book2.new(:edition => 3)
+      @sample = Book2.new
+      sleep 1
+      @target = Book2.new
     end
-    its(:title) { should be_nil }
-    its(:price) { should == 0 }
-    its(:edition) { should == 3 }
-    its(:released_at) { should == @now }
+    its(:released_at) { should_not eq @target.released_at }
   end
 end
